2234df055cbc6434f33c0e3d7723055c3a537ec6
nikhilt2412 <110010400+nikhilt2412@users.noreply.github.com>
2022-09-20 11:41:44 +0530

    1.5

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ def display_hash(Hashtable) -> None:
 			for j in range(0, len(Hashtable[i])):
 				print("--> ", end = " ")
 				print(Hashtable[i][j], end = " ")
+		print()
 
 def Hashing(keyvalue) -> int:
 	return keyvalue % len(HashTable)
