1960027ca9c715c7f8d8873fdeea37529c264194
nikhilt2412 <110010400+nikhilt2412@users.noreply.github.com>
2022-09-20 11:36:48 +0530

    1.4

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import re
 def display_hash(Hashtable) -> None:
 	# Write your code here
 	for i in range(0, len(Hashtable)):
-		if(Hashtable[keyvalue] != None):
+		if(Hashtable[i] != None):
 			print(i, end = " ")
 			for j in range(0, len(Hashtable[i])):
 				print("--> ", end = " ")
